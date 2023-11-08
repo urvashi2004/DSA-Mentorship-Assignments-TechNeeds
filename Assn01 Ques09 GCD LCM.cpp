8a25aadcd9eb490720bf2d477a26ef0dee65a95b
urvashi2004 <99309831+urvashi2004@users.noreply.github.com>
2023-11-08 21:06:14 +0530

    Corrected code

--- Assn01 Ques09 GCD LCM.cpp
+++ Assn01 Ques09 GCD LCM.cpp
@@ -1,11 +1,38 @@
 // GCD LCM
-// Code not working for LCM
 
 #include <iostream>
 using namespace std;
 
 int main()
+{// GCD LCM
+#include <iostream>
+using namespace std;
+
+int main()
 {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    for (int hcf = min(num1, num2); hcf >= 1; hcf--)
+    {
+        if ((num1 % hcf == 0) && (num2 % hcf == 0))
+        {
+            cout << "HCF = " << hcf << endl;
+            break;
+        }
+    }
+
+    for (int lcm = max(num1, num2); lcm >= max(num1, num2); lcm++)
+    {
+        if ((lcm % num1 == 0) && (lcm % num2 == 0))
+        {
+            cout << "LCM = " << lcm << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
     int num1, num2;
     cin >> num1 >> num2;
 
